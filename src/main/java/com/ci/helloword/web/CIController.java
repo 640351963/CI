9e3d239e16d9eb58ea091725f8764014b67d4261
xcb <640351963@qq.com>
2020-06-07 16:33:09 +0800

    'four'

--- src/main/java/com/ci/helloword/web/CIController.java
+++ src/main/java/com/ci/helloword/web/CIController.java
@@ -9,10 +9,6 @@ public class CIController {
     @RequestMapping("/hello")
     public  String hello(){
 
-        System.out.println("merge ");
-        System.out.println("fixme");
-        System.out.println("foru");
-        System.out.println("出现冲入了");
         return  "hello Word";
     }
 }
