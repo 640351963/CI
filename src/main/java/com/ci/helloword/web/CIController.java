326567356b597b7bfca7f2ad1d40f55246ee7324
xcb <640351963@qq.com>
2020-06-07 16:35:18 +0800

    testfour

--- src/main/java/com/ci/helloword/web/CIController.java
+++ src/main/java/com/ci/helloword/web/CIController.java
@@ -9,7 +9,7 @@ public class CIController {
     @RequestMapping("/hello")
     public  String hello(){
         System.out.println("fixme");
-
+        System.out.println("你号啊");
         return  "hello Word";
     }
 }
